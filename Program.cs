bd57093b36d56e1386c8bad15929c3b0b0f085ee
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-07 00:33:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NameSpace
+
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int br = 60;
+            Console.WriteLine(br);
+            Console.WriteLine(Storage.NekiTekst);
+            Console.WriteLine(Storage.NekiKarakter);
+
+            Storage.DirectPrint();
+        }
+    }
+}
+namespace NameSpace 
+{
+class Storage 
+    {
+      public static string NekiTekst = "Pozdrav iz NameSpace";
+      public static char NekiKarakter = 'K';
+
+        public static void DirectPrint()
+        {
+        Console.WriteLine("Ja sam metoda iz Storage klase");
+        }
+    }
+}
